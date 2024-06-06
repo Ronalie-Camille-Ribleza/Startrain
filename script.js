48d7848abf9af5ed14c2ae63cc0237c0a877f9d8
Ronalie Ribleza <84764554+Ronalie-Camille-Ribleza@users.noreply.github.com>
2024-06-07 01:50:42 +0400

    Update script.js

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ function getRandomIntInclusive(min, max)
 {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
 
 async function buttonClick()
